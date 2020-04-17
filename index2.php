9e1f26cfe9d38ef5091a4efcafefc6c1d609384b
junssource <junsdesktop@gmail.com>
2020-04-17 20:12:17 +0900

    index2.php

---
+++ index2.php
@@ -0,0 +1,40 @@
+<?php
+	$term = $_POST["term"];
+	$type = $_POST["type"];
+	$time = $_POST["time"];
+	$money = $_POST["money"];
+?>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="utf-8">
+<link href="common.css" rel="stylesheet">
+<link href="index2.css" rel="stylesheet">
+</head>
+<body>
+	<div id="container">
+		<header>
+			<div id="title">
+				생활 계산기
+			</div>
+			<div id="sub-title">
+				소비한 시간과 돈을 계산합니다.<br>
+			</div>
+		</header>
+		<div id="content">
+			이 곳에 내용을 넣어서 시작하면 됩니다.
+			내용을 넣을 수록 칸이 자동적으로 길어집니다.
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+		</div>
+	</div>
+	<footer>
+		캡스톤디자인3 15조
+	</footer>
+	<script></script>
+</body>
+</html>
